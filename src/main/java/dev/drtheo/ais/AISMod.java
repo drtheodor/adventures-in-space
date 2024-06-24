a91ff4974cef9eb3de10fc93f3cc294083752f29
DrTheodor <DrTheodor@users.noreply.github.com>
2024-06-24 22:29:18 +0300

    okay maybe now

--- src/main/java/dev/drtheo/ais/AISMod.java
+++ src/main/java/dev/drtheo/ais/AISMod.java
@@ -1,13 +1,7 @@
 package dev.drtheo.ais;
 
-import earth.terrarium.botarium.common.energy.EnergyApi;
-import earth.terrarium.botarium.common.energy.base.EnergyContainer;
-import earth.terrarium.botarium.util.Updatable;
-import loqor.ait.core.AITBlockEntityTypes;
-import loqor.ait.core.blockentities.ExteriorBlockEntity;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.entity.BlockEntity;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -17,12 +11,7 @@ public class AISMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        EnergyApi.registerEnergyBlockEntity(() -> AITBlockEntityTypes.EXTERIOR_BLOCK_ENTITY_TYPE, (level, pos, state, entity, direction) -> {
-            if (entity instanceof ExteriorBlockEntity exterior)
-                return (EnergyContainer & Updatable<BlockEntity>) exterior;
 
-            return null;
-        });
     }
 
     public static Set<BlockPos> blocksInRadius(BlockPos start, int radius) {
