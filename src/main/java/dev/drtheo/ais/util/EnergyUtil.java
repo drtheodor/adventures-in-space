26dec478c4a5c08775699d46f7ede9424d2323e8
DrTheodor <DrTheodor@users.noreply.github.com>
2024-06-25 17:41:05 +0300

    nerf

--- src/main/java/dev/drtheo/ais/util/EnergyUtil.java
+++ src/main/java/dev/drtheo/ais/util/EnergyUtil.java
@@ -2,7 +2,7 @@ package dev.drtheo.ais.util;
 
 public class EnergyUtil {
 
-    private static final int RATE = 1000;
+    private static final int RATE = 2000;
 
     public static long toBotarium(double artron) {
         return (long) (artron * RATE);
